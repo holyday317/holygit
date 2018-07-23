2d356dbc7705672a356d315fc424cbc313831dde
陈力豪 <1533901254@qq.com>
2018-07-23 10:48:14 +0800

    first commit

---
+++ holygit/holygit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  holygit
+//
+//  Created by holy on 2018/7/23.
+//  Copyright © 2018年 A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
